1057a3d07773ccd88619fb4b0311787851beae40
Nico Mkhatvari <nico.mkhatvari@gmail.com>
2015-07-20 19:01:23 +0200

    Dynamically created Knapsack and testdata objects. TODO: Place in right place  Chrono and Timer functions

--- Knapsack OpenCL/knapsack_main.cpp
+++ Knapsack OpenCL/knapsack_main.cpp
@@ -13,51 +13,57 @@
 int main(int argc, char** argv) {
     Chrono ch;
     Timer tm;
+    fstream logfile;
+    logfile.open("results_knapsack.txt", ios::out);
     //remember the limit of the 4gb allocated space
-    TestData test(1000);
+    int dim[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
     tm.start();
     ch.startChrono();
     
-    Knapsack ksack(test);
-    fstream logfile;
-    logfile.open("results_knapsack.txt", ios::out);
-    
-    //1.Query the host system for OpenCL platform info
-    ksack.queryOclPlatformInfo(&logfile);
-    //2. Query the host for OpenCL devices
-    ksack.queryOclDevice(&logfile);
+    for (int j=0; j < 10; j++) {
+        TestData* test = new TestData(dim[j]);    
+        Knapsack *ksack = new Knapsack(*test);
 
-    for (int i=0; i < ksack.getNumDevices(); i++) {
+        //1.Query the host system for OpenCL platform info
+        ksack->queryOclPlatformInfo(&logfile);
+        //2. Query the host for OpenCL devices
+        ksack->queryOclDevice(&logfile);
 
-        ksack.queryOclDeviceInfo(i, &logfile);
-        //3. Create a context and a queue to associate the OpenCL devices.
-        ksack.createContextQueue(i, &logfile);
-        //4. Create programs that will run on one or more associated devices.
-       ksack.createProgramBuild(i, &logfile);
-        //5. Create memory objects on the host or on the device.
-        //6. Copy memory data to the device as needed. Create memory objects on the host or on the device.
-       ksack.createMemObjects(&logfile);
-        //7. Create Kernels
-       ksack.createKernel();
-        //8. Provide arguments for the kernels.
-       // ksack.createKernel();
-        //9. Submit the kernels to the command queue for execution.
-        //10. Copy the results from the device to the host
-        //ksack.executeMemObjects();
-        
-        ksack.executeComputation(i, &logfile);
+        for (int i = 0; i < ksack->getNumDevices(); i++) {
 
-        ksack.printResults(&logfile);
+            ksack->queryOclDeviceInfo(i, &logfile);
+            //3. Create a context and a queue to associate the OpenCL devices.
+            ksack->createContextQueue(i, &logfile);
+            //4. Create programs that will run on one or more associated devices.
+            ksack->createProgramBuild(i, &logfile);
+            //5. Create memory objects on the host or on the device.
+            //6. Copy memory data to the device as needed. Create memory objects on the host or on the device.
+            ksack->createMemObjects(&logfile);
+            //7. Create Kernels
+            ksack->createKernel();
+            //8. Provide arguments for the kernels.
+            // ksack.createKernel();
+            //9. Submit the kernels to the command queue for execution.
+            //10. Copy the results from the device to the host
+            //ksack.executeMemObjects();
+
+            ksack->executeComputation(i, &logfile);
+
+            ksack->printResults(&logfile);
+
+        }
+        delete ksack;
+        delete test;
 
     }
     logfile.close();
     //http://stackoverflow.com/questions/14063791/double-free-or-corruption-but-why
-   
+
     ch.stopChrono();
     tm.stop();
-    cout << "TOTAL TIME: "<<ch.getTimeChrono() << endl;
-    cout << "TOTAL TIME2: "<<tm.getTime();
-    
+    cout << "TOTAL TIME: " << ch.getTimeChrono() << endl;
+    cout << "TOTAL TIME2: " << tm.getTime();
+
     return 0;
-    
+
 }
