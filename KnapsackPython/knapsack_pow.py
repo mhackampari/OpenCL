38450c98aebb33aef0887f0993b4d84c29062871
Nico Mkhatvari <nico.mkhatvari@gmail.com>
2015-10-16 19:00:22 +0200

    Fixed python_pow: now prints right results

---
+++ KnapsackPython/knapsack_pow.py
@@ -0,0 +1,121 @@
+import math
+
+f = open('datafile700', 'r')
+
+tmpArray = f.next()
+#take first line and extract second element of the array
+myCap = int(tmpArray.split()[1])
+
+print myCap
+
+items = []
+
+for line in f:
+    tmpArray = line.split()
+    items.append([int(tmpArray[-1]),int(tmpArray[1])])
+
+#items=[[35,4],[4,3],[5,4],[5,2],[7,5]] #[valore, peso]
+print("ITEMS:", items)
+
+def capacity(): return myCap#9 #constant value
+def num_obj(): return len(items) #total number of objects
+print("\nCAPACITY: ", capacity())
+print("\nNUM OBJECTS: ", num_obj())
+
+def weight_tot(item=[]):
+    sumW = 0
+    for x in item:
+        sumW += x[1]
+    return sumW
+
+sumW = weight_tot(items)
+print("\nTotal weight: ", sumW)
+c = 0 #temp var for capacity
+k=1 #temp var for obj
+wmax = 0
+
+NUMOBJ = int(math.ceil(num_obj()/32.0))
+ROWS = NUMOBJ
+COLS = capacity() + 1
+
+#initialize Memory Matrix [#obj][C]
+#for k 1..n and c 1...C
+M = [0 for x in range(COLS*ROWS)]  
+#print("M initialization:\n", M)
+
+f = [0 for c in range(capacity()+1)]
+#print("\nfunction: ", f)
+
+
+
+row = 0
+k = 0
+
+for k in range(0, len(items), 1):
+    
+    weight_k = items[k][1]
+    value_k = items[k][0]
+
+    sumW = sumW - weight_k
+    if capacity()- sumW > weight_k:
+        cmax = capacity() - sumW
+    else: cmax = weight_k
+    
+    power = k % 32
+
+    for c in range(capacity(), cmax-1, -1):
+        if f[c]<f[c - weight_k] + value_k:
+            f[c] = f[c - weight_k] + value_k
+            M[row * COLS + c] += pow(2 , power)
+       
+    if k >= 31 and k % 32 == 31:
+        row += 1
+
+
+    
+output_file = open("knapsack_pow.txt", "w")
+
+#for x in range(len(items)):
+#    print('%d: '%x , M[x])
+
+c = capacity()
+bit32 = 32
+worth = 0
+capacita = 0
+
+for i in range(NUMOBJ-1, -1, -1):
+    print("i",i) 
+    bit32 = 32
+    
+    print("**numobj%32**\n")
+    output_file.write("**numobj%32**\n")
+
+    while bit32 > 0 and c > 0:
+        print(c , bit32)
+        m = M[i*COLS + c]
+        print("m",m)
+        #print("M[%d][%d]: %d\n"%(i,c-1, M[i][c-1]))
+        #x = int(math.ceil(math.log(m,2)))
+
+        bit32pw = pow(2,(bit32-1))
+        if bit32pw == (bit32pw & m):
+
+            weight = items[i*32+bit32-1][1]
+            value = items[i*32+bit32-1][0]
+
+            #print("M[%d]: %d\n"%(i*COLS + c, M[i*COLS + c]))
+            print("[%d] value: %d \tweight: %d"%(i*32+bit32-1, value, weight))
+            output_file.write("[%d] value: %d \tweight: %d\n"%(i*32+bit32-1, value, weight))
+
+            c -= weight
+            capacita += weight
+            worth += value
+            print("KNAPSACK VALUE: ", worth)
+
+        bit32 -= 1
+
+print("\nKnapsack Value: %d"%worth)
+print("Knapsack weight: %d"%capacita)
+
+output_file.write("\nKnapsack Value: %d \nKnapsack weight: %d"%(worth, capacita))
+output_file.close
--- KnapsackPython/knapsack_toth_main.py
+++ KnapsackPython/knapsack_toth_main.py
@@ -1,9 +1,7 @@
-__author__ = "terminator"
-__date__ = "$Feb 16, 2015 7:03:15 PM$"
-
-f = open('datafile', 'r')
+f = open('datafile700', 'r')
 
 tmpArray = f.next()
+#take first line and extract second element of the array
 myCap = int(tmpArray.split()[1])
 
 print myCap
@@ -61,16 +59,33 @@ for k in range(len(items)):
     #for c in range(capacity()):
         #print("\n=============\nupdated function: ", f)
         
-#print(M)
+output_file = open("knapsack_toth.txt", "w")
+
+#for x in range(len(items)):
+#    print('%d: '%x , M[x])
+
+
 mc = 0
 mv = 0
 
 #Backtracking of the results
+print("**numobj%32**\n")
+output_file.write("**numobj%32**\n")
 c = capacity()-1
+print("selected items: ")
 for y in range(len(items)-1, -1, -1):
+    
+    if y % 32 == 31:
+        print("**numobj%32**\n")
+        output_file.write("**numobj%32**\n")
+
     for x in range(c, -1, -1):
         if(M[y][x]!=0):
-            print("selected items: ", items[y])
+            #print(items[y])
+
+            print("[%d] value: %d \tweight: %d"%(y,items[y][0],items[y][1]))
+            output_file.write("[%d] value: %d \tweight: %d\n"%(y, items[y][0],items[y][1]))
+
             c = c - items[y][1]
             mc += items[y][1]
             mv += items[y][0]
@@ -78,11 +93,12 @@ for y in range(len(items)-1, -1, -1):
         if(M[y][x]==0):
             x=0
             break
-            
-print mv 
-print mc
 
+print("\nKnapsack Value: %d"%mv)
+print("Knapsack weight: %d"%mc)
 
+output_file.write("\nKnapsack Value: %d \nKnapsack weight: %d"%(mv, mc))
+output_file.close
 
 
 
