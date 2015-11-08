db06e9639f3d36f1526dbda1b060a8cbac028233
Nico Mkhatvari <nico.mkhatvari@gmail.com>
2015-11-08 21:06:07 +0100

    "worth" and "capacita" are treated as lists; Idiomatic way to initialize M list; Added control if there are elements to compute;
    1) "worth" and "capacita" variables are conform to PyOpenCL version.
    2) Initializes in a more "pythonic" way the M list
    3) Controls at each iteration if there is any useful work to compute, in
       this way we can skip some unuseful computations.

--- KnapsackPython/knapsack_pow.py
+++ KnapsackPython/knapsack_pow.py
@@ -1,12 +1,10 @@
 import math
 
-f = open('datafile700', 'r')
+f = open('datafile', 'r')
 
 tmpArray = f.next()
 #take first line and extract second element of the array
 myCap = int(tmpArray.split()[1])
-
-print myCap
 
 items = []
 
@@ -40,8 +38,7 @@ COLS = capacity() + 1
 
 #initialize Memory Matrix [#obj][C]
 #for k 1..n and c 1...C
-M = [0 for x in range(COLS*ROWS)]  
-#print("M initialization:\n", M)
+M = [0]*(COLS*ROWS)
 
 f = [0 for c in range(capacity()+1)]
 #print("\nfunction: ", f)
@@ -52,7 +49,6 @@ row = 0
 k = 0
 
 for k in range(0, len(items), 1):
-    
     weight_k = items[k][1]
     value_k = items[k][0]
 
@@ -60,19 +56,27 @@ for k in range(0, len(items), 1):
     if capacity()- sumW > weight_k:
         cmax = capacity() - sumW
     else: cmax = weight_k
-    
-    power = k % 32
+    total_elements = capacity()-cmax+1
 
-    for c in range(capacity(), cmax-1, -1):
-        if f[c]<f[c - weight_k] + value_k:
-            f[c] = f[c - weight_k] + value_k
-            M[row * COLS + c] += pow(2 , power)
-       
+    if capacity()-cmax+1 > 0:
+        print("\nK: {0}\n".format(k))
+        print("sumW: {0} weight_k: {1} value_k: {2} k: {3} total_elements: {4}".format(sumW, weight_k, value_k, k, total_elements))
+
+        power = k % 32
+        i = 0
+        for c in range(capacity(), cmax-1, -1):
+            if f[c] < f[c - weight_k] + value_k:
+                f[c] = f[c - weight_k] + value_k
+                M[row * COLS + c] += pow(2 , power)
+            i += 1
+        #print("m_d: ", M)
+        #print("f0: ", f)
+
     if k >= 31 and k % 32 == 31:
         row += 1
 
 
-    
+
 output_file = open("knapsack_pow.txt", "w")
 
 #for x in range(len(items)):
@@ -80,42 +84,37 @@ output_file = open("knapsack_pow.txt", "w")
 
 c = capacity()
 bit32 = 32
-worth = 0
-capacita = 0
+worth = []
+capacita = []
 
-for i in range(NUMOBJ-1, -1, -1):
-    print("i",i) 
+
+
+
+for i in range(NUMOBJ-1, -1, -1
     bit32 = 32
-    
     print("**numobj%32**\n")
     output_file.write("**numobj%32**\n")
 
     while bit32 > 0 and c > 0:
         print(c , bit32)
         m = M[i*COLS + c]
-        print("m",m)
-        #print("M[%d][%d]: %d\n"%(i,c-1, M[i][c-1]))
-        #x = int(math.ceil(math.log(m,2)))
-
         bit32pw = pow(2,(bit32-1))
-        if bit32pw == (bit32pw & m):
 
+        if bit32pw == (bit32pw & m):
             weight = items[i*32+bit32-1][1]
             value = items[i*32+bit32-1][0]
-
+            #print("[%d] value:  \tweight: "%(i*32+bit32-1))
             #print("M[%d]: %d\n"%(i*COLS + c, M[i*COLS + c]))
-            print("[%d] value: %d \tweight: %d"%(i*32+bit32-1, value, weight))
             output_file.write("[%d] value: %d \tweight: %d\n"%(i*32+bit32-1, value, weight))
-
             c -= weight
-            capacita += weight
-            worth += value
-            print("KNAPSACK VALUE: ", worth)
+            capacita.append(weight)
+            worth.append(value)
 
         bit32 -= 1
 
-print("\nKnapsack Value: %d"%worth)
-print("Knapsack weight: %d"%capacita)
+print("\nKnapsack Value: ",sum(worth))
+print("Knapsack weight: ",sum(capacita))
+print("Worth aray:{0}\nWeight aray:{1}\n".format(worth,capacita))
 
-output_file.write("\nKnapsack Value: %d \nKnapsack weight: %d"%(worth, capacita))
+output_file.write("\nKnapsack Value: %s \nKnapsack weight: %s"%(str(sum(worth)), str(sum(capacita))))
 output_file.close
