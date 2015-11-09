e33a5e409fc1a9f0b1e38375a2282763773cedc2
Nico Mkhatvari <nico.mkhatvari@gmail.com>
2015-11-09 22:51:04 +0100

    Computes the algorithm on all available devices
    Polls for all available devices and computes algorithm
    on each of them, one after another.

--- KnapsackPyOpenCL/knapsack_pyopencl.py
+++ KnapsackPyOpenCL/knapsack_pyopencl.py
@@ -20,88 +20,87 @@ for device in devices:
     print("MAX_WORK_GROUP_SIZE: ",device.max_work_group_size)
     print("MAX_WORK_ITEM_SIZES: ", device.max_work_item_sizes)
 
-WG = devices[0].max_work_group_size
+    WG = device.max_work_group_size
+    context = cl.Context([device])
+    program = cl.Program(context, srcKernel)
 
-context = cl.Context(devices)
-program = cl.Program(context, srcKernel)
+    try:
+        program.build(["-cl-no-signed-zeros"])
+    except:
+        print("Build log:")
+        print(program.get_build_info(device,cl.program_build_info.LOG))
+        raise
+    else:
+        print("Program has been built!")
 
-try:
-    program.build(["-cl-no-signed-zeros"])
-except:
-    print("Build log:")
-    print(program.get_build_info(devices[0],cl.program_build_info.LOG))
-    raise
-else:
-    print("Program has been built!")
+    queue = cl.CommandQueue(context)
+    mem_flags = cl.mem_flags
 
-queue = cl.CommandQueue(context)
-mem_flags = cl.mem_flags
+    f0_mem = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, size=f0.nbytes, hostbuf=f0)
+    f1_mem = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, size=f1.nbytes, hostbuf=f1)
+    m_d_mem = cl.Buffer(context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, m_d.nbytes, hostbuf=m_d)
 
-f0_mem = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, size=f0.nbytes, hostbuf=f0)
-f1_mem = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, size=f1.nbytes, hostbuf=f1)
-m_d_mem = cl.Buffer(context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, m_d.nbytes, hostbuf=m_d)
+    kernel = cl.Kernel(program, "knapsack")
+    global_size=(NUMELEM,)
+    local_size=(WG,)
+    preferred_multiple = \
+        kernel.get_work_group_info(cl.kernel_work_group_info.PREFERRED_WORK_GROUP_SIZE_MULTIPLE, device)
 
-kernel = cl.Kernel(program, "knapsack")
-global_size=(NUMELEM,)
-local_size=(WG,)
-preferred_multiple = \
-    kernel.get_work_group_info(cl.kernel_work_group_info.PREFERRED_WORK_GROUP_SIZE_MULTIPLE, devices[0])
+    print("Kernel: ", kernel.function_name)
+    print("preferred_multiple_size: ", preferred_multiple)
 
-print("Kernel: ", kernel.function_name)
-print("preferred_multiple_size: ", preferred_multiple)
+    if WG % preferred_multiple:
+        print("Number of workers not a preferred multiple (%d*N)." \
+              % (preferred_multiple))
+        print("Performance may be reduced.")
 
-if WG % preferred_multiple:
-    print("Number of workers not a preferred multiple (%d*N)." \
-          % (preferred_multiple))
-    print("Performance may be reduced.")
+    cl.enqueue_copy(queue, f0_mem, f0, is_blocking=True)
+    cl.enqueue_copy(queue, f1_mem, f1, is_blocking=True)
+    cl.enqueue_copy(queue, m_d_mem, m_d, is_blocking=True)
 
-cl.enqueue_copy(queue, f0_mem, f0, is_blocking=True)
-cl.enqueue_copy(queue, f1_mem, f1, is_blocking=True)
-cl.enqueue_copy(queue, m_d_mem, m_d, is_blocking=True)
+    row = 0
+    k = 0
+    i = 0
+    f = numpy.zeros_like(f0)
+    M = numpy.array([]).astype(numpy.uint32)
 
-row = 0
-k = 0
-i = 0
-f = numpy.zeros_like(f0)
-M = numpy.array([]).astype(numpy.uint32)
+    for k in range(0, values.size, 1):
 
-for k in range(0, values.size, 1):
+        weight_k = weights.take(k)
+        value_k = values.take(k)
 
-    weight_k = weights.take(k)
-    value_k = values.take(k)
+        sumW = sumW - weight_k
+        if CAPACITY - sumW > weight_k:
+            cmax = CAPACITY - sumW
+        else: cmax = weight_k
+        total_elements = CAPACITY - cmax + 1
+        if total_elements > 0:
 
-    sumW = sumW - weight_k
-    if CAPACITY - sumW > weight_k:
-        cmax = CAPACITY - sumW
-    else: cmax = weight_k
-    total_elements = CAPACITY - cmax + 1
-    if total_elements > 0:
+            power = k % 32
 
-        power = k % 32
-
-        if i%2 == 0:
-            kernel.set_args(f0_mem,f1_mem, m_d_mem, numpy.uint32(cmax), weight_k, value_k, numpy.uint32(total_elements), numpy.uint32(power))
-            cl.enqueue_nd_range_kernel(queue, kernel, (CAPACITY,),None)
-        else:
-            kernel.set_args(f1_mem, f0_mem, m_d_mem, numpy.uint32(cmax), weight_k, value_k, numpy.uint32(total_elements), numpy.uint32(power))
-            cl.enqueue_nd_range_kernel(queue, kernel, (CAPACITY,),None)
-
+            if i%2 == 0:
+                kernel.set_args(f0_mem,f1_mem, m_d_mem, numpy.uint32(cmax), weight_k, value_k, numpy.uint32(total_elements), numpy.uint32(power))
+                cl.enqueue_nd_range_kernel(queue, kernel, (CAPACITY,),None)
+            else:
+                kernel.set_args(f1_mem, f0_mem, m_d_mem, numpy.uint32(cmax), weight_k, value_k, numpy.uint32(total_elements), numpy.uint32(power))
+                cl.enqueue_nd_range_kernel(queue, kernel, (CAPACITY,),None)
 
 
-        i += 1
-    if k >= 31 and k % 32 == 31:
-        row += 1
+
+            i += 1
+        if k >= 31 and k % 32 == 31:
+            row += 1
+            cl.enqueue_read_buffer(queue, m_d_mem, m_d, 0, is_blocking=True)
+            #Send back empty buffer to device
+            cl.enqueue_copy(queue, m_d_mem, numpy.zeros(CAPACITY+1).astype(numpy.uint32), is_blocking=True)
+            row += 1
+            M = numpy.append(M,m_d)
+
+
+
+    if values.size%32 != 0:
+        print("Value size is less then 32 or is not a mod of 32")
         cl.enqueue_read_buffer(queue, m_d_mem, m_d, 0, is_blocking=True)
-        #Send back empty buffer to device
-        cl.enqueue_copy(queue, m_d_mem, numpy.zeros(CAPACITY+1).astype(numpy.uint32), is_blocking=True)
-        row += 1
         M = numpy.append(M,m_d)
 
-
-
-if values.size%32 != 0:
-    print("Value size is less then 32 or is not a mod of 32")
-    cl.enqueue_read_buffer(queue, m_d_mem, m_d, 0, is_blocking=True)
-    M = numpy.append(M,m_d)
-
-myprint.printresults(M.tolist())
+    myprint.printresults(M.tolist())
